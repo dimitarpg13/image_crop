cec44f0e242e354b3853031feec18979eb33090b
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-05-28 16:40:55 -0400

    WIP: image crop manager + unit test

--- image_crop/image_crop_manager.py
+++ image_crop/image_crop_manager.py
@@ -4,9 +4,17 @@ from dichotomous_image_segmentation.segmentation_manager import\
 from PIL import Image
 
 
+def get_image_bounds_for_given_aspect_ratio(image_width, image_height, center_of_mass, aspect_ratio):
+    left = 0
+    top = 0
+    right = image_width
+    bottom = image_height
+    return left, top, right, bottom
+
+
 class ImageCropManager:
     def __init__(self):
-        pass
+        self.s_mgr = SegmentationManager(DEVICE)
 
     def get_cropped_image(self, image_path, image_name, image_file_extension, aspect_ratio):
         """
@@ -16,14 +24,12 @@ class ImageCropManager:
         :param aspect_ratio:
         :return PIL::Image instance of the cropped image
         """
-        s_mgr = SegmentationManager(DEVICE)
-        s_mask = s_mgr.predict_from_file(image_path, image_name, image_file_extension)
+
+        s_mask = self.s_mgr.predict_from_file(image_path, image_name, image_file_extension)
         c_mass = get_center_of_mass(s_mask)
 
-        left = 0
-        top = 0
-        right = s_mask.size[0]
-        bottom = s_mask.size[1]
+        left, top, right, bottom = get_image_bounds_for_given_aspect_ratio(
+            s_mask.size[0], s_mask.size[1], c_mass, aspect_ratio)
 
         image_orig = Image.open(image_path + image_name + "." + image_file_extension)
         image_cropped = image_orig.crop((left, top, right, bottom))
