fcc8d2994fbfb5269ee7b81c34636570e5422a1f
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-05-27 23:39:10 -0400

    update manager

--- image_crop/image_crop_manager.py
+++ image_crop/image_crop_manager.py
@@ -1,6 +1,5 @@
 import os
-from dichotomous_image_segmentation.\
-    dichotomous_image_segmentation.segmentation_manager import\
+from dichotomous_image_segmentation.segmentation_manager import\
     (SegmentationManager, get_center_of_mass)
 
 
