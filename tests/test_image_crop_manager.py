13dd0d8a5e0707f01f89c5c2341593fb4643178f
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-05-29 06:25:52 -0400

    update unit test

--- tests/test_image_crop_manager.py
+++ tests/test_image_crop_manager.py
@@ -7,7 +7,11 @@ from image_crop.image_crop_manager import ImageCropManager
 IC_HOME = '/'.join(Path(os.path.abspath(os.path.dirname(__file__))).parts[:-1]).replace('/','',1)
 DATASET_PATH = 'tests/sample_data/'
 
-param_list = [('smiling-man', 3.0, 4.0, 308, 410),('smiling-man', 4.0, 3.0, 547, 410)]
+param_list = [('smiling-man', 3.0, 4.0, 308, 410),
+               ('smiling-man', 4.0, 3.0, 547, 410),
+               ('basketball_players', 3.0, 4.0, 744, 992),
+               ('basketball_players', 3.0, 5.0, 595, 992),
+               ('kitchen-bar', 3.0, 4.0, 454, 606)]
 
 
 class TestImageCropManager(unittest.TestCase):
@@ -20,7 +24,7 @@ class TestImageCropManager(unittest.TestCase):
             with self.subTest(image_name=image_name, aspect_ratio_a=aspect_ratio_a, aspect_ratio_b=aspect_ratio_b,
                               cropped_image_width=cropped_image_width, cropped_image_height=cropped_image_height):
                 cropped_image = self.ic_manager.get_cropped_image(image_path, image_name, 'jpeg',
-                                                                  cropped_image_width/cropped_image_height)
+                                                            aspect_ratio_a/aspect_ratio_b)
 
                 self.assertEqual(cropped_image.size[0], cropped_image_width)
                 self.assertEqual(cropped_image.size[1], cropped_image_height)
